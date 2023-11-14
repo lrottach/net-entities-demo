ceff17fff79d82c917b5e36c24d0f06b73de3972
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2023-11-14 14:01:14 +0100

    feat: Add Durable Task and Http extensions, remove solution file, and add http client to dependency injection.

--- src/Program.cs
+++ src/Program.cs
@@ -4,4 +4,10 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .Build();
 
+// Add http client to dependency injection
+host.Services.AddHttpClient("AzureApiClient", configuration =>
+        {
+            configuration.BaseAddress = new Uri("https://management.azure.com/");
+        });
+
 host.Run();
